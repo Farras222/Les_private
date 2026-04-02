a374b207c6b1114676e295eb4ac78383e298dbc1
Farras222 <tfrasyy@gmail.com>
2026-04-03 06:53:40 +0700

    Add files via upload

---
+++ les_private/views/pages/guru/siswa/tambah.php
@@ -0,0 +1,144 @@
+<?php 
+include '../index.php'; 
+// Tambah user baru jika form disubmit
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $role = 'siswa';
+    $phone = $_POST['phone'];
+    $alamat = $_POST['alamat'];
+
+    $query = "INSERT INTO user (name, email, password, role, no_hp, alamat) 
+              VALUES ('$name', '$email', '$password', '$role', '$phone', '$alamat')";
+    mysqli_query($mysqli, $query);
+
+    // Redirect ke halaman user setelah penambahan
+    header('Location: siswa.php');
+    exit();
+}
+?>
+
+
+   <style>
+    /* Agar form search tetap bagus di berbagai posisi */
+#navbar .input-group {
+    min-width: 300px;
+    max-width: 500px;
+}
+
+/* Responsif */
+@media (max-width: 768px) {
+    #navbar .input-group {
+        min-width: 100%;
+        max-width: 100%;
+    }
+    
+    #navbar .btn-primary {
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+}
+
+/* Agar tombol dan input sejajar */
+#navbar .input-group-text,
+#navbar .form-control,
+#navbar .btn {
+    height: 38px;
+}
+.avatar-initial {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #5e72e4; /* bebas, sesuaikan tema */
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 14px;
+  text-transform: uppercase;
+  padding: 8px;
+  margin-right: 10px;
+}
+
+   </style>
+  
+
+<body class="g-sidenav-show bg-warning">
+  <!-- sidebar -->
+  <?php include '../../../components/sidebar.php'; ?>  
+    <main class="main-content position-relative border-radius-lg ">
+      <!-- Navbar -->
+      <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+        <div class="container-fluid py-1 px-3">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+              <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages / Guru</a></li>
+              <li class="breadcrumb-item text-sm text-white active" aria-current="page">Siswa</li>
+            </ol>
+            <h6 class="font-weight-bolder text-white mb-0">Siswa</h6>
+          </nav>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+    
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4 p-4">
+                    <form method="POST">
+                        <div class="card-title pb-2">
+                            <h3>Tambah Siswa</h3>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" placeholder="Nama Lengkap" class="form-control" id="name" name="name" required />
+                            </div>
+                            </div>
+                            <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                            </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" placeholder="Password" class="form-control" id="password" name="password" required />
+                            </div>
+                            </div>
+                            <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="phone">No. Hp</label>
+                                <input type="phone" placeholder="No. Hp" class="form-control" id="phone" name="phone" />
+                            </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="alamat">Alamat</label>
+                                <input type="text" placeholder="Alamat" class="form-control" id="alamat" name="alamat" />
+                            </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-success mt-3">Simpan</button>
+                        <a href="user.php" class="btn btn-danger mt-3">Batal</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <?php include '../../../components/footer.php'; ?>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
